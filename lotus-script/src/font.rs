c4d55792776e387c13457b2fedd70189a4a920c7
Marvin Lee Fimmel <kontakt@ekranos.me>
2025-05-19 06:31:29 +0200

    improved: interface of BitmapFont

--- lotus-script/src/font.rs
+++ lotus-script/src/font.rs
@@ -3,43 +3,46 @@ use lotus_shared::content::ContentId;
 pub use lotus_shared::font::*;
 
 /// A bitmap font that can be used to render text.
-pub struct BitmapFont(ContentId);
+pub struct BitmapFont {
+    content_id: ContentId,
+    properties: BitmapFontProperties,
+}
 
 impl BitmapFont {
-    /// Create a new bitmap font from a content id.
-    pub fn new(content_id: ContentId) -> Self {
-        Self(content_id)
-    }
-
-    /// Get the properties of this font.
+    /// Try to load a bitmap font from a content id.
     /// Returns `None` if the font is not currently loaded. It will be loaded in the background.
     /// Just call this function again later until it returns `Some`.
-    pub fn properties(&self) -> Option<BitmapFontProperties> {
-        let font = FfiObject::new(&self.0);
+    pub fn try_load(content_id: ContentId) -> Option<Self> {
+        let font = FfiObject::new(&content_id);
         let properties = unsafe { lotus_script_sys::font::bitmap_font_properties(font.packed()) };
 
         if properties == 0 {
             None
         } else {
-            Some(FfiObject::from_packed(properties).deserialize())
+            let properties = FfiObject::from_packed(properties).deserialize();
+            Some(Self {
+                content_id,
+                properties,
+            })
         }
     }
 
+    /// Get the properties of this font.
+    pub fn properties(&self) -> &BitmapFontProperties {
+        &self.properties
+    }
+
     /// Get the width of the text in pixels.
-    /// Returns `None` if the font is not currently loaded. It will be loaded in the background.
-    /// Just call this function again later until it returns `Some`.
-    pub fn text_len(&self, text: &str, letter_spacing: i32) -> Option<u32> {
-        let font = FfiObject::new(&self.0);
+    pub fn text_len(&self, text: &str, letter_spacing: i32) -> u32 {
+        let font = FfiObject::new(&self.content_id);
         let text = FfiObject::new(&text);
 
         let len = unsafe {
             lotus_script_sys::font::text_len(font.packed(), text.packed(), letter_spacing)
         };
 
-        if len < 0 {
-            None
-        } else {
-            Some(len as u32)
-        }
+        assert!(len >= 0);
+
+        len as u32
     }
 }
