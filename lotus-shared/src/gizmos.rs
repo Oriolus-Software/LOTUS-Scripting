80ddf9bc7404c8dcd81fa4333baedd8cc8298bbe
Marvin Lee Fimmel <kontakt@ekranos.me>
2025-05-21 09:17:22 +0200

    added: constructors for Gizmo

--- lotus-shared/src/gizmos.rs
+++ lotus-shared/src/gizmos.rs
@@ -12,6 +12,7 @@ pub enum GizmoKind {
 /// A gizmo to draw in the game.
 /// A gizmo is a visual indicator of something in the game.
 /// It is used to help with debugging and development.
+/// Gizmos are drawn relative to the center of the object the script is attached to.
 #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub struct Gizmo {
     pub kind: GizmoKind,
@@ -19,6 +20,44 @@ pub struct Gizmo {
 }
 
 impl Gizmo {
+    /// Create a new gizmo.
+    pub fn new(kind: GizmoKind, color: Color) -> Self {
+        Self { kind, color }
+    }
+
+    /// Create a new wire cube gizmo.
+    pub fn wire_cube(center: impl Into<Vec3>, half_extents: impl Into<Vec3>, color: Color) -> Self {
+        Self::new(
+            GizmoKind::WireCube {
+                center: center.into(),
+                half_extents: half_extents.into(),
+            },
+            color,
+        )
+    }
+
+    /// Create a new wire sphere gizmo.
+    pub fn wire_sphere(center: impl Into<Vec3>, radius: impl Into<f32>, color: Color) -> Self {
+        Self::new(
+            GizmoKind::WireSphere {
+                center: center.into(),
+                radius: radius.into(),
+            },
+            color,
+        )
+    }
+
+    /// Create a new arrow gizmo.
+    pub fn arrow(start: impl Into<Vec3>, end: impl Into<Vec3>, color: Color) -> Self {
+        Self::new(
+            GizmoKind::Arrow {
+                start: start.into(),
+                end: end.into(),
+            },
+            color,
+        )
+    }
+
     /// Draw the gizmo.
     #[cfg(feature = "ffi")]
     pub fn draw(&self) {
