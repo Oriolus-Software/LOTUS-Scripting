7bb427530a6b9e77548242348a9b7670f13f4985
Marcel Kuhnt <mkuhnt@oriolus-software.de>
2025-05-30 12:26:47 +0200

    moved exponential approach to lotus-script

--- lotus-script/src/math.rs
+++ lotus-script/src/math.rs
@@ -1,1 +1,8 @@
+use crate::time::delta;
+
 pub use lotus_shared::math::*;
+
+pub fn exponential_approach(old_value: f32, exponent: f32, target: f32) -> f32 {
+    let factor = 1.0 - (-delta() * exponent).exp();
+    old_value + factor * (target - old_value)
+}
