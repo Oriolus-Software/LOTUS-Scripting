ae41abbc54e33bf3d55303dc688fbdce30289ff6
Marcel Kuhnt <mkuhnt@oriolus-software.de>
2025-10-09 23:14:35 +0200

    added exponential approach for 64 bit

--- lotus-script/src/math.rs
+++ lotus-script/src/math.rs
@@ -1,8 +1,13 @@
-use crate::time::delta;
+use crate::time::{delta, delta_f64};
 
 pub use lotus_shared::math::*;
 
 pub fn exponential_approach(old_value: f32, exponent: f32, target: f32) -> f32 {
     let factor = 1.0 - (-delta() * exponent).exp();
+    old_value + factor * (target - old_value)
+}
+
+pub fn exponential_approach_64(old_value: f64, exponent: f64, target: f64) -> f64 {
+    let factor = 1.0 - (-delta_f64() * exponent).exp();
     old_value + factor * (target - old_value)
 }
